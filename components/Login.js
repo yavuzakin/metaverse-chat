758d8a9a1fc4fa659c4277109f3f4c56242df41a
yavuzakin <yavuzakin55@gmail.com>
2022-01-14 19:56:36 +0300

    Changed logo inside login page and header component and removed logout button

--- components/Login.js
+++ components/Login.js
@@ -5,26 +5,22 @@ function Login() {
   const { authenticate } = useMoralis();
   return (
     <div className="bg-black relative">
-      <div className="z-50 flex flex-col absolute items-center justify-center h-4/6 w-full space-y-4">
+      <div className="z-50 flex flex-col absolute items-center justify-center h-4/6 w-full space-y-10">
         <Image
           className="object-cover rounded-full"
-          src="https://links.papareact.com/3pi"
-          width={200}
-          height={200}
+          src="/metaverse-logo.jpg"
+          width={240}
+          height={240}
         />
         <button
           onClick={authenticate}
-          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+          className="bg-cyan-300 rounded-lg p-5 font-bold animate-pulse"
         >
           Login to the METAVERSE
         </button>
       </div>
       <div className="w-full h-screen">
-        <Image
-          src="https://links.papareact.com/55n"
-          layout="fill"
-          objectFit="cover"
-        />
+        <Image src="/login-bg.jpeg" layout="fill" objectFit="cover" />
       </div>
     </div>
   );
