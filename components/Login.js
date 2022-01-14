af50e259cd2150615aabf5f490fcee2b552e80a9
yavuzakin <yavuzakin55@gmail.com>
2022-01-14 18:17:53 +0300

    Login screen modified

--- components/Login.js
+++ components/Login.js
@@ -1,21 +1,30 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 function Login() {
+  const { authenticate } = useMoralis();
   return (
     <div className="bg-black relative">
       <div className="z-50 flex flex-col absolute items-center justify-center h-4/6 w-full space-y-4">
         <Image
           className="object-cover rounded-full"
-          src="/login-logo.png"
+          src="https://links.papareact.com/3pi"
           width={200}
           height={200}
         />
-        <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+        >
           Login to the METAVERSE
         </button>
       </div>
       <div className="w-full h-screen">
-        <Image src="/login-bg.jpeg" layout="fill" objectFit="cover" />
+        <Image
+          src="https://links.papareact.com/55n"
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
     </div>
   );
